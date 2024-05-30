d413fc55c9ef2988abdaaa7150e422fe733a4a65
mraychaudhuri <mayukh@operationspark.org>
2024-05-30 21:02:18 +0000

    changed color and added moon background

--- projects/runtime/js/view/background.js
+++ projects/runtime/js/view/background.js
@@ -39,11 +39,17 @@ var background = function (window) {
             // TODO 1:
             // this currently fills the background with an obnoxious yellow;
             // you should modify both the height and color to suit your game
-            var backgroundFill = draw.rect(canvasWidth,canvasHeight,'yellow');
+            var backgroundFill = draw.rect(canvasWidth,groundY,'aquamarine');
             background.addChild(backgroundFill);
             
             // TODO 2: - Add a moon and starfield
             
+            var moon = draw.bitmap("img/moon.png");
+            moon.x = 800;
+            moon.y = 50;
+            moon.scaleX = 0.3;
+            moon.scaleY = 0.3;
+            background.addChild(moon);
             
             // TODO 4: Part 1 - Add buildings!     Q: This is before TODO 4 for a reason! Why?
             
