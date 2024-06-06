937405fa6fb7ba851f45e3420fbf58cc351756f5
mraychaudhuri <mayukh@operationspark.org>
2024-06-06 20:56:24 +0000

    saving to do's 1-5

--- asd-projects/snake/index.js
+++ asd-projects/snake/index.js
@@ -11,7 +11,9 @@ var highScoreElement = $("#highScore");
 
 // TODO 4a: Create the snake, apple and score variables
 // Game Variables
-
+var snake = {};
+var apple = {};
+var score = 0;
 // Constant Variables
 var ROWS = 20;
 var COLUMNS = 20;
@@ -41,11 +43,17 @@ init();
 
 function init() {
   // TODO 4c-2: initialize the snake
+  // initialize the snake's body as an empty Array
+    snake.body = [];
 
+ // make the first snakeSquare and set it as the head
+    makeSnakeSquare(10, 10);
+    snake.head = snake.body[0];
   // TODO 4b-2: initialize the apple
-
+    makeApple();
   // TODO 5a: Initialize the interval
-
+    // start update interval
+    updateInterval = setInterval(update, 100);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -58,6 +66,15 @@ function init() {
  */
 function update() {
   // TODO 5b: Fill in the update function's code block
+    moveSnake();
+
+    if (hasHitWall() || hasCollidedWithSnake()) {
+      endGame();
+    }
+
+    if (hasCollidedWithApple()) {
+      handleAppleCollision();
+    }
 }
 
 function checkForNewDirection(event) {
@@ -185,6 +202,16 @@ function endGame() {
  */
 function makeApple() {
   // TODO 4b-1: Fill in the makeApple() code block
+  apple.element = $("<div>").addClass("apple").appendTo(board);
+
+  // get a random available row/column on the board
+   var randomPosition = getRandomAvailablePosition();
+   // initialize the row/column properties on the Apple Object
+   apple.row = randomPosition.row;
+   apple.column = randomPosition.column;
+
+   // position the apple on the screen
+   repositionSquare(apple);
 }
 
 /* Create an HTML element for a snakeSquare using jQuery. Then, given a row and
@@ -193,6 +220,27 @@ function makeApple() {
  */
 function makeSnakeSquare(row, column) {
   // TODO 4c-1: Fill in this function's code block
+       // initialize a new snakeSquare Object
+       var snakeSquare = {};
+    
+       // make the snakeSquare.element Object and append it to the board
+       snakeSquare.element = $("<div>").addClass("snake").appendTo(board);
+    
+       // initialize the row and column properties on the snakeSquare Object
+       snakeSquare.row = row;
+       snakeSquare.column = column;
+    
+       // set the position of the snake on the screen
+       repositionSquare(snakeSquare);
+    
+       // if this is the head, add the snake-head id
+       if (snake.body.length === 0) {
+         snakeSquare.element.attr("id", "snake-head");
+       }
+    
+       // add snakeSquare to the end of the body Array and set it as the new tail
+       snake.body.push(snakeSquare);
+       snake.tail = snakeSquare;
 }
 
 /* 
