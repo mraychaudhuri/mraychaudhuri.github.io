83f03abe60921e661b2b8e14255eeb1fa05cd8d3
Mayukh Raychaudhuri <mayukh@operationspark.org>
2025-06-20 21:07:47 +0000

    Finished todo 3

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -12,11 +12,17 @@ function runProgram(){
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
   
   // Game Item Objects
-
-
+  
+  const KEY = {
+    ENTER: 13,
+    LEFT: 37,
+    UP: 38,
+    RIGHT: 39,
+    DOWN: 40,
+  };
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -34,8 +40,20 @@ function runProgram(){
   /* 
   Called in response to events.
   */
-  function handleEvent(event) {
-
+  function handleKeyDown(event) {
+    console.log(event.which);
+    if (event.which === KEY.LEFT) {
+    console.log("left pressed");
+    }
+    if (event.which === KEY.RIGHT) {
+    console.log("right pressed");
+    }
+    if (event.which === KEY.UP) {
+    console.log("up pressed");
+    }
+    if (event.which === KEY.DOWN) {
+    console.log("down pressed");
+    }
   }
 
   ////////////////////////////////////////////////////////////////////////////////
